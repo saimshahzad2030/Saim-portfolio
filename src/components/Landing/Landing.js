3745580c99e1cec1d99e1121181ff109f7956806
saimshehzad2030 <saimshehzad2030@gmail.com>
2024-04-02 00:55:32 +0500

    final

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -26,22 +26,22 @@ const Landing = () => {
            <div className={`absolute w-full flex flex-col items-start ${style.textDiv}  p-4 rounded-lg`}>
            {textArray.map((text, index) => (
             
-            <h1 className={` text-sm w-9/12  left-0 ${style.textItem} transform transition-opacity bg-gray-50 duration-500 text-gray-700 text-center px-4 py-4 rounded-md 2xl:text-xl 2xl:w-4/12 xl:text-xl xl:w-7/12 lg:text-xl lg:w-6/12  sm:text-lg sm:w-6/12  font-bold mb-4 ${style.textItem} ${index === textIndex ? style.active : ''}`} key={index} >
-              {text}
-            </h1>
+            <h1 className={`text-xs w-9/12 left-0 ${style.textItem} transform transition-opacity ${index=== 1 || index === 3?'text-gray-50':'text-gray-700'}  duration-500  text-center px-4 py-4 rounded-md 2xl:text-xl 2xl:w-4/12 xl:text-3xl xl:w-8/12 lg:text-2xl lg:w-8/12 sm:text-xl sm:w-8/12 font-bold mb-4 ${style.textItem} ${index === textIndex ? style.active : ''}`} key={index}>
+            {text}
+          </h1>
           ))}
           <div className={` flex flex-row items-center ${style.socials}  p-4 rounded-lg`}>
   <Link href={'https://www.linkedin.com/in/saim-shahzad-476bba25b/'}>
-                            <img className={`bg-gray-50 h-4 w-4 2xl:h-16 2xl:w-16 xl:h-8 xl:w-8 lg:h-8 lg:w-8 sm:h-8 sm:w-8  sm:flex sm:h-4 w-4 rounded-full cursor-pointer mr-3 2xl:mr-8 xl:mr-5 lg:mr-6 sm:mr-6  transition-transform transform hover:scale-105 ${style.icon1}`} src="/Assets/socials/linkedin.png" alt="Facebook" />
+                            <img className={`bg-gray-50 h-6 w-6 2xl:h-16 2xl:w-16 xl:h-8 xl:w-8 lg:h-8 lg:w-8 sm:h-8 sm:w-8  sm:flex sm:h-4 w-4 rounded-full cursor-pointer mr-3 2xl:mr-8 xl:mr-5 lg:mr-6 sm:mr-6  transition-transform transform hover:scale-105 ${style.icon1}`} src="/Assets/socials/linkedin.png" alt="Facebook" />
                         </Link>
                         <Link href={'https://github.com/saimshahzad2030'}>
-                            <img className={`bg-gray-50 h-4 w-4 2xl:h-16 2xl:w-16 xl:h-8 xl:w-8 lg:h-8 lg:w-8 sm:h-8 sm:w-8  sm:flex h-4 w-4 rounded-full cursor-pointer mr-3 2xl:mr-8 xl:mr-5 lg:mr-6  sm:mr-6 transition-transform transform hover:scale-105 ${style.icon2}`} src="/Assets/socials/github.png" alt="Facebook" />
+                            <img className={`bg-gray-50 h-6 w-6 2xl:h-16 2xl:w-16 xl:h-8 xl:w-8 lg:h-8 lg:w-8 sm:h-8 sm:w-8  sm:flex h-4 w-4 rounded-full cursor-pointer mr-3 2xl:mr-8 xl:mr-5 lg:mr-6  sm:mr-6 transition-transform transform hover:scale-105 ${style.icon2}`} src="/Assets/socials/github.png" alt="Facebook" />
                         </Link>
                         <Link href={'https://www.facebook.com/sheikh.saim.10'}>
-                            <img className={`bg-gray-50 h-4 w-4 2xl:h-16 2xl:w-16 xl:h-8 xl:w-8 lg:h-8 lg:w-8 sm:h-8 sm:w-8  sm:flex h-4 w-4 rounded-full cursor-pointer  mr-3 2xl:mr-8 xl:mr-5 lg:mr-6  sm:mr-6 transition-transform transform hover:scale-105 ${style.icon3}`} src="/Assets/socials/facebook.png" alt="Facebook" />
+                            <img className={`bg-gray-50 h-6 w-6 2xl:h-16 2xl:w-16 xl:h-8 xl:w-8 lg:h-8 lg:w-8 sm:h-8 sm:w-8  sm:flex h-4 w-4 rounded-full cursor-pointer  mr-3 2xl:mr-8 xl:mr-5 lg:mr-6  sm:mr-6 transition-transform transform hover:scale-105 ${style.icon3}`} src="/Assets/socials/facebook.png" alt="Facebook" />
                         </Link>
                         <Link href={'https://twitter.com/saimshehzad10'}>
-                            <img className={`bg-gray-50 h-4 w-4 2xl:h-16 2xl:w-16 xl:h-8 xl:w-8 lg:h-8 lg:w-8 sm:h-8 sm:w-8 sm:flex h-4 w-4 rounded-full cursor-pointer mr-3 2xl:mr-8 xl:mr-5 lg:mr-6  sm:mr-6 transition-transform transform hover:scale-105 ${style.icon4}`} src="/Assets/socials/twitter.png" alt="Facebook" />
+                            <img className={`bg-gray-50 h-6 w-6 2xl:h-16 2xl:w-16 xl:h-8 xl:w-8 lg:h-8 lg:w-8 sm:h-8 sm:w-8 sm:flex h-4 w-4 rounded-full cursor-pointer mr-3 2xl:mr-8 xl:mr-5 lg:mr-6  sm:mr-6 transition-transform transform hover:scale-105 ${style.icon4}`} src="/Assets/socials/twitter.png" alt="Facebook" />
                         </Link>
                       </div>
           </div>
